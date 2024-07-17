d0747c2d68fd6b54aa160dda814a11a51224839f
darlin <darlin.gomez@tecnologicoliceocristiano.edu.ec>
2024-07-16 20:08:26 -0500

    Matrix

---
+++ LaMatrix.py
@@ -0,0 +1,10 @@
+#Escribe un programa que solicite al usario 
+#que ingrese su nombre.El nombre de almacenar 
+#en una variable llamada nombre
+#a continuación se debe mostrar en pantalla 
+#al texto"Ahora esta en la matrix,[usuario]", donde
+#"[usuario]" se reemplazara por el nonbre que
+#el usuario haya ingresado
+
+nombre=input("Escribe tu nombre: ")
+print("Ahora esta en la matrix, " + nombre)
